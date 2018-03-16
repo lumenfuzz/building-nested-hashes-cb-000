03fc37f96e5ca9a74dc7636dcac86b1ef970f3bf
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-16 22:16:17 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -18,7 +18,7 @@ def fourth_challenge
           name: "Steven",
           age: "30",
           attitude: "confused",
-        }
+        },
         {
           name: "Nurse",
           age: "44",
